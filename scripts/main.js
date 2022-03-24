6363ef087f4c75e9241304be62d0de4db8df2a03
SantanaC4 <eduardo.sanati@gmail.com>
2022-03-24 00:30:48 -0300

    update scripts

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,12 @@ const meuBotao = document.querySelector('button');
 meuBotao.addEventListener("click", getUser);
 
 
-function getUser() {   //Search for github user 
+function getUser() {  
     var gitUser = document.querySelector('input')
-    fetch(`https://api.github.com/users/${gitUser.value}`)
-    .then(response => response.json()) //Converting the response to a JSON object
+    fetch(`https://api.github.com/users/${gitUser.value}`)  //Search for github user 
+    .then(response => {
+      return response.json()
+    }) //Converting the response to a JSON object
     .then(function(data) {
       if(data.message){
           console.log('User Profile Not Found')
@@ -17,12 +19,14 @@ function getUser() {   //Search for github user
           document.getElementById('res').innerHTML = `
           ${data.name}`
       }
-      fetch(`https://api.github.com/users/USER_GITHUB/${gitUser.value}/repos`)
-      .then(response => response.json()) //Converting the response to a JSON object
-      .then( data =>{
+      return fetch(`https://api.github.com/users/USER_GITHUB/${gitUser.value}/repos`)
+    })
+    .then(response => {
+      return response.json()
+    }) //Converting the response to a JSON object
+    .then(data =>{
           console.log(data)
           document.getElementById('rep').innerHTML = `${data}`
-      })
     })
     .catch(function(error) {
       console.log(error)
