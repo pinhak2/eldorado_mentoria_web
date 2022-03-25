c3928f6d4e5275c5ae8c229a7ed4a70a91f2d758
SantanaC4 <eduardo.sanati@gmail.com>
2022-03-25 10:32:56 -0300

    primeira versão

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const meuCabecalho = document.querySelector('h1');
+const meuCabecalho = document.getElementById('initial-text');
 meuCabecalho.textContent = 'Github Profile Search';
 
 const meuBotao = document.querySelector('button');
@@ -6,6 +6,7 @@ meuBotao.addEventListener("click", getUser);
 
 
 function getUser() {  
+    document.getElementById('list-rep').innerHTML = '';
     var gitUser = document.querySelector('input')
     fetch(`https://api.github.com/users/${gitUser.value}`)  //Search for github user 
     .then(response => {
@@ -16,21 +17,30 @@ function getUser() {
           console.log('User Profile Not Found')
       }else{
           console.log(data)
-          document.getElementById('res').innerHTML = `
-          ${data.name}`
+          document.getElementById('res').innerHTML = `${data.name}`
       }
-      return fetch(`https://api.github.com/users/${gitUser.value}/repos`)
+      return fetch(`https://api.github.com/users/${gitUser.value}/repos`) // Search for Repositories
     })
     .then(response => {
       return response.json()  //Converting the response to a JSON object
     })
     .then(data =>{
           console.log(data)
-          document.getElementById('rep').innerHTML = `${data.length}`
+          document.getElementById('rep').innerHTML = putRepo(data) // Display repositories list
     })
     .catch(function(error) {
       console.log(error)
     });
 }
 
+function putRepo(data) {
+  var listDiv = document.getElementById("list-rep");
+  var ul=document.createElement('ul');
+  for (var i = 0; i < data.length; ++i) {
+    var li=document.createElement('li');
+    li.innerHTML = data[i].name;
+    ul.appendChild(li);
+  }
+  listDiv.appendChild(ul);
+}
 var myImage = document.querySelector('img');
