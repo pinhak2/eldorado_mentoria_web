30942afebe2bb62de7edcc24039a30125fd9acd7
Guilherme Martins <gui.martins.soares@gmail.com>
2022-05-11 11:15:39 -0300

    Removidos comentários desnecessários, logs, etc

--- docs/guilherme.martins/modulo1/main.js
+++ docs/guilherme.martins/modulo1/main.js
@@ -1,94 +1,81 @@
-//document.querySelector('html').onclick = function() {
-    //alert('Ai! Pare de me cutucar!');
-//}
-
 const project_name = [];
 const lngg = [];
 const vsblt = [];
 const webadd = [];
 
-let myimg = document.querySelector('img');
-myimg.onclick = function() {
-	let mysrc = myimg.getAttribute('src');
-	if (mysrc === 'search.png')
-	{
-		myimg.setAttribute('src','Pacha.png');
-	}
-		else
-		{
-		myimg.setAttribute('src','search.png');
-		}
-}
-
-const searchInput = document.getElementById('submit_btn');
-
-searchInput.addEventListener("click", getUserInfo);
+let header_img = document.querySelector("img");
+header_img.onclick = function () {
+  let header_src = header_img.src.slice(-10);
+  if (header_src === "search.png") {
+    header_img.setAttribute("src", "Pacha.png");
+  } else {
+    header_img.setAttribute("src", "search.png");
+  }
+};
 
 
-//searchInput.addEventListener("input", (e) => {
-	//let value = e.target.value 
-	
-	async function getUserInfo(){
-	const usrnm = document.getElementById('search').value;
-	const url = "https://api.github.com/users/" + usrnm 
-	try{
-		const response = await fetch(url)
-		
-		if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-		}
-	const result = await response.json()
-	var full_name = result.name
-	var avatar_img = result.avatar_url
-	var cmpny = result.company
-	var flwrs = result.followers
-	var lctn = result.location
-	} catch(e) {
-		console.log(e)
-	}
-	
-	
-	const repos = url + '/repos'
-	try{
-		const response2 = await fetch(repos)
-		
-		if (!response2.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-		}
-	const result2 = await response2.json()
-	result2.forEach(i => {
-	project_name.push(i.name);
-	lngg.push(i.language);
-	vsblt.push(i.visibility);
-	webadd.push(i.html_url);})
-	console.log(webadd);
-	console.log(lngg);
-	console.log(vsblt);
-	console.log(full_name);
-	console.log(result2)
-	} catch(e2) {
-		console.log(e2)
-	}
-	
-	var newwin = window.open('git_repo.html', '_blank')
-	newwin.onload = function(){
-		this.full_name = full_name;
-		this.avatar_img = avatar_img;
-		this.cmpny = cmpny;
-		this.flwrs = flwrs;
-		this.lctn = lctn;
-		this.project_name = project_name;
-		this.lngg = lngg;
-		this.vsblt = vsblt;
-		this.webadd = webadd;
-		this.get();
-	}	
-	
-	
-	
+
+async function getUserInfo() {
+  const username = document.getElementById("search").value;
+  const url = `https://api.github.com/users/${username}`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const result = await response.json();
+    var full_name = result.name;
+    var avatar_img = result.avatar_url;
+    var cmpny = result.company;
+    var flwrs = result.followers;
+    var lctn = result.location;
+  } catch (e) {
+    console.log(e);
+  }
+
+  const repository = `${url}/repos`;
+  try {
+    const responseRepository = await fetch(repository);
+
+    if (!responseRepository.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const resultRepository = await responseRepository.json();
+    resultRepository.forEach((i) => {
+      project_name.push(i.name);
+      lngg.push(i.language);
+      vsblt.push(i.visibility);
+      webadd.push(i.html_url);
+    });
+  } catch (e2) {
+    console.log(e2);
+  }
+
+  var newwin = window.open("git_repo.html", "_blank");
+  newwin.onload = function () {
+    this.full_name = full_name;
+    this.avatar_img = avatar_img;
+    this.cmpny = cmpny;
+    this.flwrs = flwrs;
+    this.lctn = lctn;
+    this.project_name = project_name;
+    this.lngg = lngg;
+    this.vsblt = vsblt;
+    this.webadd = webadd;
+    this.get();
+  };
+
+  cleanPrevious();
 }
 
 
 
+function cleanPrevious(){
+  const caixa = document.getElementById("caixa");
+  const botan = document.getElementById("submit_btn");
 
- 
+  caixa.remove();
+  botan.remove();
+}
+
