1d754f18943e69b1f5f413a7ea82fd92a28b97f3
SantanaC4 <eduardo.sanati@gmail.com>
2022-05-13 10:36:27 -0300

    change condition of the for loop in the main.js

--- docs/eduardo.santana/modulo_1/scripts/main.js
+++ docs/eduardo.santana/modulo_1/scripts/main.js
@@ -33,9 +33,9 @@ function getUser() {
 function putRepo(data) {
 	var listDiv = document.getElementById('list-rep');
 	var ul=document.createElement('ul');
-	for (var i = 0; i < data.length; ++i) {
+	for (let object of data) {
 		var li=document.createElement('li');
-		li.innerHTML = data[i].name;
+		li.innerHTML = object.name;
 		ul.appendChild(li);
 	}
 	listDiv.appendChild(ul);
