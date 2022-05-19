bf62367aa1020a1f00ba094b234d7ed6c7e45d64
Guilherme Martins <gui.martins.soares@gmail.com>
2022-05-19 16:09:34 -0300

    Melhorias e simplificações
    Separação de responsabilidades de funções;
    Realizar pesquisa ao digitar enter;
    Abrir projeto em nova aba;
    Adição de mensagem de erro;
    Padronização de variáveis;

---
+++ docs/guilherme.martins/modulo_1/main.js
@@ -0,0 +1,99 @@
+//----------------Variables-------------------------------------
+const url = "https://api.github.com/users/";
+let imageChangeFlag = true;
+//--------------------------------------------------------------
+
+function easterEgg(headerImg) {
+  headerImg.src = `./images/${imageChangeFlag ? "pacha" : "search"}.png`;
+  imageChangeFlag = !imageChangeFlag;
+}
+
+function search() {
+  event.preventDefault();
+  const username = document.getElementById("searchText").value;
+  hideElements(".error, form, .profile, .repositories");
+  getUserInfo(username)
+    .then((userInfo) => {
+      displayProfile(userInfo);
+      return getUserRepo(username);
+    })
+    .then((userRepo) => displayRepos(userRepo))
+    .catch((_) => {
+      showElement(".error", "Não foi possivel encontrar usuário!");
+    })
+    .finally(() => {
+      showElement("footer");
+    });
+}
+
+function hideElements(querySelector) {
+  const elements = [...document.querySelectorAll(querySelector)];
+  for (const element of elements) {
+    element.style.display = "none";
+  }
+}
+
+function showElement(querySelector, html = false) {
+  const element = document.querySelector(querySelector);
+  element.style.display = "block";
+  if (html !== false) {
+    element.innerHTML = html;
+  }
+}
+
+function displayProfile({ name, avatar_url, company, followers, location }) {
+  showElement(
+    ".profile",
+    `
+      <div class="imageContainer">
+        <img src="${avatar_url}" alt="Avatar do usuário do github" />
+      </div>
+
+      <div class="profileContainer">
+        <p><label>Nome Completo:</label> ${name}</p>
+        <p><label>Localização:</label> ${location}</p>
+        <p><label>Empresa:</label> ${company}</p>
+        <p><label>Número de Seguidores:</label> ${followers}</p>
+      </div>
+    `
+  );
+}
+
+function displayRepos(repositories) {
+  showElement(
+    ".repositories",
+    repositories
+      .map(({ html_url, name, visibility, language }) => {
+        return `
+            <div class='project'>
+              <a target='_blank' href='${html_url}'>${name}</a> 
+              <p>${visibility}</p>
+              <p>${language}</p>
+            </div>
+          `;
+      })
+      .join("")
+  );
+}
+
+function backToSearch() {
+  showElement("form");
+  hideElements("footer, .profile, .repositories, .error");
+  document.getElementById("searchText").value = "";
+}
+
+async function getUserInfo(username) {
+  const response = await fetch(`${url}${username}`);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+}
+
+async function getUserRepo(username) {
+  const response = await fetch(`${url}${username}/repos`);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+}
